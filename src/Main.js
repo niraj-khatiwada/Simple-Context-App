e0575f2fb52cce1386deea8dce58aa2eba3c1e4b
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-04-21 13:19:38 +0545

    Refactored for using functional Component

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 
 import styles from './Styles/MainStyles'
 
 import { ThemeContext } from './Contexts/ThemeContext'
 
-class Main extends Component {
-  static contextType = ThemeContext
-  render() {
-    const { classes } = this.props
-    return (
-      <div
-        className={classes.main}
-        style={{ backgroundColor: this.context.isDark ? 'black' : 'white' }}
-      >
-        {this.props.children}
-      </div>
-    )
-  }
+function Main(props) {
+  const { isDark } = useContext(ThemeContext)
+  const { classes } = props
+  return (
+    <div
+      className={classes.main}
+      style={{ backgroundColor: isDark ? 'black' : 'white' }}
+    >
+      {props.children}
+    </div>
+  )
 }
 
 export default withStyles(styles)(Main)
